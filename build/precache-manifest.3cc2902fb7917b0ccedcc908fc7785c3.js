2f62f4d3cd4dd5902067790ae7e5c0f76bc3b38a
rem029 <elawrenceponce@gmail.com>
2020-07-21 16:48:05 +0300

    Updated for GitPages

---
+++ build/precache-manifest.3cc2902fb7917b0ccedcc908fc7785c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dedd70e9b4178d1dacef6e5cb6c09d2",
+    "url": "/SocialMediaCard/index.html"
+  },
+  {
+    "revision": "5435b559e315f81615a0",
+    "url": "/SocialMediaCard/static/css/main.91e506f6.chunk.css"
+  },
+  {
+    "revision": "35f8dbb1afb966ef8d8a",
+    "url": "/SocialMediaCard/static/js/2.c12e9f33.chunk.js"
+  },
+  {
+    "revision": "5435b559e315f81615a0",
+    "url": "/SocialMediaCard/static/js/main.23a5692b.chunk.js"
+  },
+  {
+    "revision": "0ef5c80d6bf667df1587",
+    "url": "/SocialMediaCard/static/js/runtime~main.af2dc3e6.js"
+  }
+]);
